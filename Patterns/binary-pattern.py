ba2f73ba45afad940cb5a7bf9d7a737d35272301
Christina George <145782012+christinawdc@users.noreply.github.com>
2026-06-17 11:16:27 +0530

    Update binary-pattern.py

--- Patterns/binary-pattern.py
+++ Patterns/binary-pattern.py
@@ -6,3 +6,21 @@ for i in range(1,n+1):
         else:
             print("0",end="")
     print()
+# Sample Input 
+# 5
+# Sample Output 
+# 10101
+# 01010
+# 10101
+# 01010
+# 10101
+
+# Sample Input 
+# 6
+# Sample Output 
+# 101010
+# 010101
+# 101010
+# 010101
+# 101010
+# 010101
