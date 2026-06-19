9d0f081abb3b3130b32867b0756495630327cf91
Christina George <145782012+christinawdc@users.noreply.github.com>
2026-06-19 12:34:42 +0530

    Update 75. Sort Colors.py

--- Leetcode/75. Sort Colors.py
+++ Leetcode/75. Sort Colors.py
@@ -1,5 +1,4 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         red,white,blue=nums.count(0),nums.count(1),nums.count(2)
-        nums.clear()
-        nums.extend(red*[0]+white*[1]+blue*[2])
+        nums[:]=(red*[0]+white*[1]+blue*[2])
