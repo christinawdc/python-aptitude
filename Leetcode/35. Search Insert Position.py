d3bcbd329a10ad0b732ef142631a4e357d37af39
Christina George <145782012+christinawdc@users.noreply.github.com>
2026-07-30 13:26:30 -0500

    Update 35. Search Insert Position.py

--- Leetcode/35. Search Insert Position.py
+++ Leetcode/35. Search Insert Position.py
@@ -3,12 +3,10 @@ class Solution:
         low,high=0,len(nums)-1
         while low<=high:
             mid=low+ (high-low)//2
-            if target>nums[mid]:
+            if target==nums[mid]:
+                return mid
+            elif target>nums[mid]:
                 low=mid+1
-                continue
             elif target<nums[mid]:
                 high=mid-1
-                continue
-            elif target==nums[mid]:
-                return mid
         return low
