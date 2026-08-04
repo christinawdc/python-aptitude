8abf831986b6539414f83153a0874bc636035501
Christina George <145782012+christinawdc@users.noreply.github.com>
2026-08-04 05:20:03 -0500

    Create 367. Valid Perfect Square.py

---
+++ Leetcode/367. Valid Perfect Square.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num==1:
+            return True
+        low,high=1,num//2
+        while low<=high:
+            mid=(high+low)//2
+            if mid*mid==num:
+                return True
+            elif mid*mid<num:
+                low=mid+1
+            else: 
+                high=mid-1
+        return False
