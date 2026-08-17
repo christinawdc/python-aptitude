bd9037e0b5cae3fed0e91f5a16341eac0783e40b
Christina George <145782012+christinawdc@users.noreply.github.com>
2026-08-17 04:06:38 -0500

    Create 119. Pascal's Triangle II.py

---
+++ Leetcode/119. Pascal's Triangle II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        list2=[]
+        for i in range(rowIndex+1):
+            list1=[1]
+            if i!=0:
+                prev=list2[-1]
+                for j in range(len(prev)-1):
+                    list1.append(prev[j]+prev[j+1])
+                list1.append(1)
+            list2.append(list1)
+        return list2[-1]
